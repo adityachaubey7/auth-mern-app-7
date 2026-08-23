5a869da8961e05408bb98b33e95217b2c428a0e3
Aditya Chaubey <chaubeyaditya252@gmail.com>
2026-08-23 23:18:12 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -25,7 +25,7 @@ function Signup() {
       return handleError("name,email and password are required");
     }
     try {
-      const url = "https://auth-mern-app-7-api.vercel.app//auth/signup";
+      const url = "https://auth-mern-app-7-api.vercel.app/auth/signup";
       const response = await fetch(url, {
         method: "POST",
         headers: {
