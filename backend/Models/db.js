dd07798e51897e8d76b23d64b490e1b9561f2b74
Aditya Chaubey <chaubeyaditya252@gmail.com>
2026-08-24 01:27:11 +0530

    Update db.js

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -1,11 +1,10 @@
-const mongoose =require('mongoose')
+const mongoose = require("mongoose");
 
-const mongo_url=process.env.MONGO_CONN;
-mongoose.connect(process.env.MONGO_CONN)
-mongoose.connect(mongo_url)
-    .then(()=>{
-        console.log("mongodb connected... ")
-    }).catch((err)=>{
-        console.log("mongodb connection error",err)
-    })
-
+mongoose
+  .connect(process.env.MONGO_CONN)
+  .then(() => {
+    console.log("mongodb connected...");
+  })
+  .catch((err) => {
+    console.log("mongodb connection error", err);
+  });
